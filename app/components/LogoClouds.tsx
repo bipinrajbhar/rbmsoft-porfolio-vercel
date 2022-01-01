680198b4030d631fa88b2ab21a37309ed6c24a9e
Bipin Rajbhar <brajbhar@rh.com>
2022-01-02 00:40:43 +0530

    added more spacing in logo-clouds

--- app/components/LogoClouds.tsx
+++ app/components/LogoClouds.tsx
@@ -3,17 +3,17 @@ import RHBabyAndChildLogo from "../../public/images/brands/rh-baby-and-child.svg
 
 export default function LogoClouds() {
   return (
-    <section className="my-6">
-      <p className="text-gray-700 font-semibold text-center tracking-wider mb-6">
+    <section className="mt-10 mb-6">
+      <p className="mb-6 font-semibold tracking-wider text-center text-gray-700">
         TRUSTED BY 2 FORWARD-THINKING COMPANIES
       </p>
       <div className="grid grid-cols-4 gap-0.5">
-        <div className="bg-gray-50 flex items-center justify-center p-8">
+        <div className="flex items-center justify-center p-8 bg-gray-50">
           <a href="https://rh.com/" target="_blank">
             <img src={RHLogo} alt="RH" />
           </a>
         </div>
-        <div className="bg-gray-50 flex items-center justify-center p-8">
+        <div className="flex items-center justify-center p-8 bg-gray-50">
           <a href="https://rhbabyandchild.rh.com/" target="_blank">
             <img src={RHBabyAndChildLogo} alt="RH Baby & Child" />
           </a>
